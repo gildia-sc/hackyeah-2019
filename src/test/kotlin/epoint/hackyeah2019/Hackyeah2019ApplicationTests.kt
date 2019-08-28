bf83d0015539f12d5876f73477f72a4c5435b876
Tomasz Tarczyński <tarczynskitomek@gmail.com>
2019-08-28 18:30:17 +0200

    Add basic dependencies

---
+++ src/test/kotlin/epoint/hackyeah2019/Hackyeah2019ApplicationTests.kt
@@ -0,0 +1,16 @@
+package epoint.hackyeah2019
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class Hackyeah2019ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
