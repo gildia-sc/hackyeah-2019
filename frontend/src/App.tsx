08b75011c5609d15e0d818a4a1581349edbb7146
mstar95 <m.star@wp.pl>
2019-09-04 23:40:56 +0200

    add truffle and metamask integration

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import getWeb3 from "./utils/getWeb3";
 
 const App: React.FC = () => {
+  getWeb3();
   return (
     <div className="App">
       <header className="App-header">
---
+++ frontend/src/utils/getWeb3.ts
@@ -0,0 +1,19 @@
+const Web3 = require("web3");
+const wind:any = window as any;
+
+export default async () => {
+    const ethereum = wind.ethereum;
+    if (ethereum) {
+        const web3 = new Web3(ethereum);
+        try {
+            // Request account access if needed
+            await ethereum.enable();
+            // Acccounts now exposed
+            const accounts = await web3.eth.getAccounts();
+            console.log(accounts);
+            return web3;
+        } catch (error) {
+            console.log(error)
+        }
+    }
+}
