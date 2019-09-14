3df2b0b7c0ab281e0cbef1d2f2d13729b34234dc
Zbigniew Barczyk <zqbany@gmail.com>
2019-09-15 01:58:50 +0200

    Add home icon with root link

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,6 +10,17 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import NewProductSentView from "./send-new/NewProductSentView";
+import SvgIcon from '@material-ui/core/SvgIcon';
+
+function HomeIcon(props) {
+  return (
+    <a href="/">
+    <SvgIcon {...props}>
+      <path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z" />
+    </SvgIcon>
+    </a>
+  );
+}
 
 function App() {
     return (
@@ -18,7 +29,7 @@ function App() {
 
                 <AppBar position="static">
                     <Toolbar>
-                        <Typography variant="h6">Choose Eco</Typography>
+                        <Typography variant="h6">Choose Eco</Typography>&nbsp;&nbsp;<HomeIcon color="action" />
                     </Toolbar>
                 </AppBar>
                 <Route exact path="/" component={ProductScannerView}/>
