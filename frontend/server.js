8fae75665d52892c7ec2552c8db79be4a656639f
mstar95 <m.star@wp.pl>
2019-09-14 17:11:56 +0200

    express server

---
+++ frontend/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+var path = require('path');
+var fs = require('fs');
+var https = require('https');
+
+var privateKey = fs.readFileSync('privatekey.pem');
+var certificate = fs.readFileSync('server.crt');
+var credentials = {key: privateKey, cert: certificate};
+
+var app = express();
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/build/index.html'));
+});
+
+var httpsServer = https.createServer(credentials, app);
+
+httpsServer.listen(3000, 'localhost', (err) => {
+    if (err) {
+        console.log(err);
+        return;
+    }
+
+    console.log('Listening at https://localhost:3000');
+});
