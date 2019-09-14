b8da92abc1ca2c5fc148902cf1f4014920747364
Adam Molewko <adam.molewko@orange.com>
2019-09-14 23:20:10 +0200

    product details navigation

--- frontend/src/scanner/ProductListElement.js
+++ frontend/src/scanner/ProductListElement.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import Grid from "@material-ui/core/Grid";
+import {Link} from "react-router-dom";
+import {productDetected} from "./ScannerReducer";
+import {useDispatch} from "react-redux";
 
 function ProductListElement({product}) {
-    console.log(product)
+    console.log(product);
+    const dispatch = useDispatch();
     return (
-        < div>
+        <div>
+            <Grid item>
+                <Link to={"/product/" + product.ean} onClick={() => dispatch(productDetected(product.ean))}>
+                    <img src={"/demo-ean-codes/test_barcode_" + product.ean + "_300x169.jpg"}
+                         alt={"Click to select '" + product.name + "'"}/>
+                </Link>
+            </Grid>
+
             {
-                product.ean
+                product.name
             }
         </div>
     )
