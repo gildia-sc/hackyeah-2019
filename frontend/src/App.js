704caae2c8496ba07c65b1efc70503de910da597
Marek Berkan <mberkan@e-point.pl>
2019-09-14 20:21:17 +0200

    Bazowe dane produktu

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,15 +4,25 @@ import ProductScannerView from "./scanner/ProductScannerView";
 import ProductDetailsView from "./product/ProductDetalisView";
 import {BrowserRouter as Router, Route} from "react-router-dom"
 
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+
 function App() {
     return (
         <Router>
             <div className="App">
-                <header className="App-header">
-                    <p>
-                        Choose Eco
-                    </p>
-                </header>
+
+                <AppBar position="static">
+                    <Toolbar>
+                        {/*<IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">*/}
+                        {/*    <MenuIcon />*/}
+                        {/*</IconButton>*/}
+                        <Typography variant="h6">
+                            Choose Eco
+                        </Typography>
+                    </Toolbar>
+                </AppBar>
                 <Route exact path="/" component={ProductScannerView}/>
                 <Route path="/product/:ean" component={ProductDetailsView}/>
             </div>
--- frontend/src/product/ProductDetalisView.js
+++ frontend/src/product/ProductDetalisView.js
@@ -2,6 +2,8 @@ import {useDispatch, useSelector} from "react-redux"
 import React, {useEffect} from "react"
 import {fetchProduct} from "./ProductsReducer"
 
+import Grid from '@material-ui/core/Grid';
+
 function ProductDetailsView({history, match}) {
     const ean = match.params.ean
     const dispatch = useDispatch();
@@ -12,8 +14,39 @@ function ProductDetailsView({history, match}) {
     }, [ean])
     return (
         <>
-            { product  && product.ean}
+            { product && (
+                <Grid container spacing={3}>
+                    <Grid item xs={6}>Product name:</Grid>
+                    <Grid item xs={6}>{product.name}</Grid>
+                    <Grid item xs={6}>Company:</Grid>
+                    <Grid item xs={6}>{product.company}</Grid>
+                    <Grid item xs={6}>Scoring:</Grid>
+                    <Grid item xs={6}>???</Grid>
+                    <Grid item xs={6}>Category:</Grid>
+                    <Grid item xs={6}>
+                        TODO {/*{product.category.name}*/}
+                    </Grid>
+                    <Grid item xs={6}>Generated waste:</Grid>
+                    <Grid item xs={6}>TODO: {product.productWeight} + {product.packageWeight} / {product.productWeight}</Grid>
+                    <Grid item xs={6}>Shipping distance:</Grid>
+                    <Grid item xs={6}>TODO: {product.latitude} / {product.longitude}</Grid>
+                    <Grid item xs={6}>Weight with packaging:</Grid>
+                    <Grid item xs={6}>{product.productWeight} + {product.packageWeight}</Grid>
+                    <Grid item xs={6}>Package material type:</Grid>
+                    <Grid item xs={6}>
+                        TODO: {/*{product.packageMaterial.code}*/}
+                    </Grid>
+                    <Grid item xs={6}>Package material recycling percent:</Grid>
+                    <Grid item xs={6}>
+                        TODO {/*{product.packageMaterial.recyclePotential}%*/}</Grid>
+                    <Grid item xs={6}>Package biodegradate time:</Grid>
+                    <Grid item xs={6}>
+                        TODO {/*{product.packageMaterial.timeToBiodegradateInDays} (days)*/}
+                    </Grid>
+                </Grid>
+            )}
         </>)
 }
 
 export default ProductDetailsView
+
