40aa6e355bd8758ef54e2e65103ecc0769b84cd0
Zbigniew Barczyk <zqbany@gmail.com>
2019-09-14 14:06:52 +0200

    Added sample populating from json file - alpha without foreign keys

---
+++ src/main/java/pl/hackyeah2019/hackyeah/configuration/GimmeMeBeans.java
@@ -0,0 +1,17 @@
+package pl.hackyeah2019.hackyeah.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.data.repository.init.Jackson2RepositoryPopulatorFactoryBean;
+
+@Configuration
+class GimmeMeBeans {
+    @Bean
+    Jackson2RepositoryPopulatorFactoryBean respositoryPopulator() {
+        Jackson2RepositoryPopulatorFactoryBean factory = new Jackson2RepositoryPopulatorFactoryBean();
+        factory.setResources(new Resource[]{new ClassPathResource("products.json")});
+        return factory;
+    }
+}
