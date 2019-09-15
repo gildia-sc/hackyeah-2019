9750a658d942538583dfcffde1f89f3b9bbe77cc
mstar95 <m.star@wp.pl>
2019-09-15 12:56:30 +0200

    working reverse proxy

--- frontend/server.js
+++ frontend/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 var path = require('path');
 var fs = require('fs');
 var https = require('https');
+var proxy = require('http-proxy-middleware');
 
 var privateKey = fs.readFileSync('privatekey.pem');
 var certificate = fs.readFileSync('server.crt');
@@ -10,6 +11,15 @@ var credentials = {key: privateKey, cert: certificate};
 var app = express();
 
 app.use(express.static(path.join(__dirname, 'build')));
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+//app.use(proxy('/products/**', {target: 'http://ec2-18-220-161-116.us-east-2.compute.amazonaws.com:8080', changeOrigin: true}));
+
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/public/index.html'));
--- frontend/src/better_product/BetterProductApi.js
+++ frontend/src/better_product/BetterProductApi.js
@@ -1,4 +1,4 @@
-const URL = '/products/search/findTopByCategoryNameAndScoreGreaterThanOrderByScoreDesc'
+const URL = 'https://e0ce076a.ngrok.io/products/search/findTopByCategoryNameAndScoreGreaterThanOrderByScoreDesc'
 
 async function get(category, score) {
     const data = await fetch(`${URL}?categoryName=${category}&score=${score}&projection=ProductProjection`)
