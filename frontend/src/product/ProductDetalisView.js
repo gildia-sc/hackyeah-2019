6d1856bd47ff18bc8390a56a3f229365ed68ffc1
Marek Berkan <mberkan@e-point.pl>
2019-09-15 10:44:52 +0200

    Liczenie odległości od miejsca produkcji na podstawie koordynatów GPS

--- frontend/src/product/ProductDetalisView.js
+++ frontend/src/product/ProductDetalisView.js
@@ -13,6 +13,7 @@ function ProductDetailsView({history, match}) {
     const ean = match.params.ean
     const dispatch = useDispatch();
     const products = useSelector(state => state.products.products);
+    const coords = useSelector(state => state.gpsReducer.coords);
     const product = products.find(p => p.ean === ean);
     const humanizer = require('humanize-duration')
     const [scoring, setScoring] = React.useState(0);
@@ -77,9 +78,35 @@ function ProductDetailsView({history, match}) {
     function humanizeDate(days) {
         return humanizer(days*24*60*60*1000);
     }
+
+    function getDistanceFromLatLonInKm(lat1, lon1, lat2, lon2) {
+        var R = 6371; // Radius of the earth in km
+        var dLat = deg2rad(lat2-lat1);  // deg2rad below
+        var dLon = deg2rad(lon2-lon1);
+        var a =
+            Math.sin(dLat/2) * Math.sin(dLat/2) +
+            Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
+            Math.sin(dLon/2) * Math.sin(dLon/2)
+        ;
+        var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+        var d = R * c; // Distance in km
+        return d;
+    }
+
+    function deg2rad(deg) {
+        return deg * (Math.PI/180)
+    }
+
     function calculateDistance(product) {
-        // TODO: calculate basing on GPS coordinate
-        return Math.round(product.longitude * 100);
+        if(coords) {
+            console.log("Use coords")
+            console.log(coords.latitude)
+            console.log(coords.longitude)
+            return Math.round(getDistanceFromLatLonInKm(coords.latitude, coords.longitude, product.latitude, product.longitude));
+        } else {
+            console.log("No coords -> skip")
+            return "?";
+        }
     }
     return (
         <>
