46ce887305d0ffbd7578bccf60663a6d821c94c9
mstar95 <m.star@wp.pl>
2019-09-15 12:56:30 +0200

    working reverse proxy

--- frontend/src/better_product/BetterProductApi.js
+++ frontend/src/better_product/BetterProductApi.js
@@ -1,4 +1,4 @@
-const URL = 'https://e6bb9d70.ngrok.io/products/search/findTopByCategoryNameAndScoreGreaterThanOrderByScoreDesc'
+const URL = '/products/search/findTopByCategoryNameAndScoreGreaterThanOrderByScoreDesc'
 
 async function get(category, score) {
     const data = await fetch(`${URL}?categoryName=${category}&score=${score}&projection=ProductProjection`)
